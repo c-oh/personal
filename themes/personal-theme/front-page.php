1c839d83e22ba5260f23a413a9e3f7f9231fb8e2
hollythedev <hollythedev@users.noreply,github.com>
2017-05-17 15:15:52 -0700

    first commit

---
+++ themes/personal-theme/front-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package my-site_Theme
+ */
+get_header(); ?>
+
+<div class= "fp-banner">
+<p class = "fp-subtitle">is a web developer and UI designer.</p>
+</div>
