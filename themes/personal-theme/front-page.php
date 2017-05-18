6d89077610cff0a752ebd73d29357cacaf6abba6
hollythedev <papiniu@outlook.com>
2017-05-18 15:30:50 -0700

    add scroll down icon

--- themes/personal-theme/front-page.php
+++ themes/personal-theme/front-page.php
@@ -7,7 +7,9 @@
 get_header(); ?>
 
 <div class= "fp-banner">
+    <h4 class= "fp-title">Holly Oi Hang Cheng</h4>
 <p class = "fp-subtitle">is a web developer.</p>
+<img src="<?php echo get_template_directory_uri() . '/resources/images/mouse_down.png'; ?>" alt="Scroll down icon" />
 </div> 
 
 <?php get_footer();?>
